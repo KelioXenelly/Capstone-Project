b598634cc85dd7bb27643b5b5ea10413942d9c83
Kelio Xenelly <kelioxenelly@Keliooos-Mac.local>
2026-03-13 12:24:01 +0700

    Finishing Section 2 Course 1

--- Course_1/next-js/05-onwards-foodies-starting-project/app/meals/page.js
+++ Course_1/next-js/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,5 +1,19 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -19,7 +33,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
